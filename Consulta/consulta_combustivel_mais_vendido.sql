6df2c16a72102cd332810e08ba133f100381468b
Mª Clara Arruda <maria.almeida@serpro.gov.br>
2019-11-04 13:36:07 -0300

    alterações

--- Consulta/consulta_combustivel_mais_vendido.sql
+++ Consulta/consulta_combustivel_mais_vendido.sql
@@ -1,7 +1,10 @@
 -- 3- Qual o tipo de combustível mais vendido em um posto X.
-SELECT p.nome_fantasia, pc.id_combustivel, c.nome FROM Posto as p
-INNER JOIN Posto_combustivel as pc 
-ON pc.cnpj = p.cnpj
-INNER JOIN Combustivel as c
-ON c.id_combustivel = pc.id_combustivel
-GROUP BY pc.id_combustivel
+-- SELECT p.nome_fantasia, MAX(pc.id_combustivel) FROM Posto as p
+-- INNER JOIN Posto_combustivel as pc
+-- ON pc.cnpj = p.cnpj
+-- GROUP BY p.nome_fantasia
+
+-- SELECT cnpj, id_combustivel, MAX(conta) FROM (SELECT pc.cnpj, pc.id_combustivel, count(pc.id_combustivel) as conta FROM Posto_combustivel as pc
+-- GROUP BY pc.cnpj, pc.id_combustivel) temp
+-- GROUP BY cnpj, id_combustivel
+-- HAVING
