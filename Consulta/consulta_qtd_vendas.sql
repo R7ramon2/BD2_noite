89c504a8d38e7a984a2d2c874a78ce53f4bd92c8
Mª Clara Arruda <maria.almeida@serpro.gov.br>
2019-11-04 10:20:58 -0300

    consulta 1 reformulada

---
+++ Consulta/consulta_qtd_vendas.sql
@@ -0,0 +1,4 @@
+-- 1- Quantidade de vendas, de um posto X, realizadas em um determinado tempo.
+-- do mês Junho p/ cima
+SELECT COUNT(pr.momento) from Preco as pr
+where pr.momento >= "2019-06-01 00:00:00"
