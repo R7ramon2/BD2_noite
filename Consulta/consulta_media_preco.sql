7b7508f9296fb31f377d7692e5647f11b622506f
rubensarruda <rubens_96a@hotmail.com>
2019-11-04 22:29:08 -0300

    Consultas atualizadas

--- Consulta/consulta_media_preco.sql
+++ Consulta/consulta_media_preco.sql
@@ -1,1 +1,7 @@
 -- 5- Qual a média de preço de combustivel em um determinado tempo.
+SELECT AVG(v.valor_litro) as Media FROM Vende as v
+INNER JOIN Posto_combustivel as pc
+ON pc.id_posto_combustivel = v.id_posto_combustivel
+INNER JOIN Preco as pr
+ON pr.id_preco = pc.id_preco
+WHERE pr.momento >= "2019-06-01 00:00:00"
--- Consulta/consulta_posto_mais_vendas.sql
+++ Consulta/consulta_posto_mais_vendas.sql
@@ -1,2 +1,10 @@
--- 4- Qual o posto que mais vendeu em um determinado tempo.
+-- 4-  Qual o posto que mais vendeu em um determinado tempo.
+SELECT p.nome_fantasia as NomeDoPosto, COUNT(p.nome_fantasia) as Vezes from Posto as p
+INNER JOIN Posto_combustivel as pc
+ON pc.cnpj = p.cnpj
+INNER JOIN Preco as pr
+ON pr.id_preco = pc.id_preco
+where pr.momento >= "2019-06-01 00:00:00"
+GROUP BY p.nome_fantasia
+ORDER BY Vezes DESC 
 
