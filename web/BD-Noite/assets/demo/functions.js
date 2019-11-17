3155eb9b8d043ee0104ee03e84d9ac6ccb8f9b99
Ramon Ranieri <ramonranieri@hotmail.com>
2019-11-17 19:36:10 -0300

    Correção de bugs

--- web/BD-Noite/assets/demo/functions.js
+++ web/BD-Noite/assets/demo/functions.js
@@ -94,3 +94,36 @@ function log_out(){
     alert("Usuário deslogado");
     window.location.href = "../pages/Login/";
 }
+
+function chart1(){
+
+    if ($('#dailySalesChart').length != 0 && $('#websiteViewsChart').length != 0) {
+    // ----------==========     Daily Sales Chart initialization For Documentation    ==========---------- //
+
+    dataDailySalesChart = {
+      labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+      series: [
+        [100, 17, 7, 17, 23, 18, 38]
+      ]
+    };
+
+    optionsDailySalesChart = {
+      lineSmooth: Chartist.Interpolation.cardinal({
+        tension: 0
+      }),
+      low: 0,
+      high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+      chartPadding: {
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0
+      },
+    }
+
+    var dailySalesChart = new Chartist.Line('#dailySalesChart', dataDailySalesChart, optionsDailySalesChart);
+
+    var animationHeaderChart = new Chartist.Line('#websiteViewsChart', dataDailySalesChart, optionsDailySalesChart);
+
+  }
+}
